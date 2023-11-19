d1b17be679ab9ef189e46722aed6735794c53eb2
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2023-11-19 15:19:13 +0100

    finishing ch2.5

--- parser/parser.go
+++ parser/parser.go
@@ -26,6 +26,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -49,6 +51,24 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 
 	for !p.curTokenIs(token.SEMICOLON) {
 		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+
+	p.nextToken()
+
+	// TODO: we're skipping the expressions
+	// until we encounter a semicolon
+
+	for !p.curTokenIs(token.SEMICOLON) {
+
+		p.nextToken()
+
 	}
 
 	return stmt
