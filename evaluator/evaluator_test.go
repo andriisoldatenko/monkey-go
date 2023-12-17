3a04381b56f7070c6082f761d002e2f5afcd0716
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2023-12-17 19:54:01 +0100

    add string concatination

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -183,6 +183,10 @@ if (10 > 1) {
 			"foobar",
 			"identifier not found: foobar",
 		},
+		{
+			`"Hello" - "World"`,
+			"unknown operator: STRING - STRING",
+		},
 	}
 
 	for _, tt := range tests {
@@ -280,6 +284,20 @@ func TestStringLiteral(t *testing.T) {
 	evaluated := testEval(input)
 
 	str, ok := evaluated.(*object.String)
+	if !ok {
+		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	if str.Value != "Hello World!" {
+		t.Errorf("String has wrong value. got=%q", str.Value)
+	}
+}
+
+func TestStringConcatenation(t *testing.T) {
+	input := `"Hello" + " " + "World!"`
+	evaluated := testEval(input)
+	str, ok := evaluated.(*object.String)
+
 	if !ok {
 		t.Fatalf("object is not String. got=%T (%+v)", evaluated, evaluated)
 	}
