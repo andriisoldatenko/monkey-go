f63af854cee2be3c26525fdf9c9ea0f0f9fef4d2
Andrii Soldatenko <andrii.soldatenko@dynatrace.com>
2024-06-02 18:19:47 +0200

    implement generic index

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -474,6 +474,27 @@ func TestHashLiterals(t *testing.T) {
 	runCompilerTests(t, tests)
 }
 
+func TestIndexExpressions(t *testing.T) {
+	tests := []compilerTestCase{
+		{
+			input:             "{1:2}[2-1]",
+			expectedConstants: []any{1, 2, 2, 1},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpConstant, 1),
+				code.Make(code.OpHash, 2),
+				code.Make(code.OpConstant, 2),
+				code.Make(code.OpConstant, 3),
+				code.Make(code.OpSub),
+				code.Make(code.OpIndex),
+				code.Make(code.OpPop),
+			},
+		},
+	}
+
+	runCompilerTests(t, tests)
+}
+
 func runCompilerTests(t *testing.T, tests []compilerTestCase) {
 	t.Helper()
 
